f455726289e71ffd5a23528ba9d366b24d9cead7
sameerpandya <sameer.pandya@gmail.com>
2017-08-29 20:20:55 -0400

    Initial commit

---
+++ app/src/main/java/com/spandya/android/sharedphonebook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.spandya.android.sharedphonebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
